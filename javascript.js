0d88a2298cc8dc32b4e3d639cc15484145e88512
mince <59406036+elzh814@users.noreply.github.com>
2021-04-10 18:14:54 -0400

    :(

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 var isDay = true;
+var isRaining = false;
 function playMusic(audio) {
     var music = document.getElementById(audio);
     //music.play();
@@ -11,20 +12,31 @@ function playMusic(audio) {
 }
 
 function dayCycle(changeToDay) {
-    startRain(false);
     var overlayGrad = document.getElementById('overlay');
     if (changeToDay) {
         overlayGrad.style.opacity = "0.0";
         isDay = true;
+        isRaining = false;
+        //document.getElementById("Rain").pause();
+        stop();
     }
     else if (!changeToDay && isDay) {
         overlayGrad.style.opacity = "0.5";
         //var t = setInterval(changeOverlay,200, 0.05, overlayGrad);
         isDay = false;
+        isRaining = true;
+        //document.getElementById("Sun").pause();
+        startRain(false);
+        //setTimeout(function(){startRain(false);},1000);
+        //setTimeout(function(){startRain(false);},2000);
     }
     else if (!changeToDay && !isDay) {
         overlayGrad.style.opacity = "0.0";
         isDay = true;
+        isRaining = false;
+        //document.getElementById("Sun").pause();
+        startRain(false);
+        stopRain();
     }
 }
 
@@ -39,14 +51,43 @@ function changeOverlay(num, element) {
     }
 }
 
-window.onload = function() {
+window.onload = function () {
     document.getElementById('overlay').style.opacity = "0.0";
 }
 
 function startRain(isRaining) {
     if (!isRaining) {
-        var newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'rain');
-        document.getElementById("train").appendChild(newDiv);
+        isRaining = true;
+        var count = 0;
+
+        while (count < 50) {
+            var newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'rain');
+            let randLeft = Math.random() * (1500 - 300) + 300;
+            let randTop = Math.random() * (100 - (-400)) - 400;
+
+            let root = getComputedStyle(document.querySelector(':root'));
+            let tempTop = root.getPropertyValue('--top');
+            tempTop = randTop;
+            newDiv.style.top = tempTop + "px";
+            newDiv.style.left = randLeft + "px";
+            
+            let newRoot = document.documentElement;
+            let result = tempTop + 80;
+            root.style.setAttribute('--finalTop', result + "%");
+            console.
+            
+            
+
+            document.getElementById("window").appendChild(newDiv);
+            count++;
+        }
+    }
+}
+
+function stopRain() {
+    var x = document.getElementsByClassName("rain");
+    while (x[0]) {
+        x[0].parentNode.removeChild(x[0]);
     }
 }
